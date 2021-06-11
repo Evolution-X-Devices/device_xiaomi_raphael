baceabea92660dac1eb6d36cc7bc95cd6e217544
Joey Huab <joey@evolution-x.org>
2021-06-11 15:27:20 +0900

    raphael: Initialize Evolution X v5.8
    * Import missing changes from https://github.com/Evolution-X-Devices/device_xiaomi_raphael/commits/elle-backup.
    
    Revert "raphael: Set overlay for custom doze package"
    
    This reverts commit b74651ac4cf22391f3dab087f46be9bf91549e5e.
    
    Revert "raphael: Rename doze target"
    
    This reverts commit 78b673f20141e9165484ca230b6d15824cc42672.
    
    Revert "raphael: Deprecate XiaomiDoze"
    
    This reverts commit 3f07540719f5c584379eb9c2ef0aeb7bfb654010.
    
    Revert "[SQUASH] raphael: switch to CustomDoze"
    
    This reverts commit 049073604c738fcc21f0b2a6e612c643b056735c.
    
    Revert "raphael: parts: Allow to wake the screen instead of pulsing"
    
    This reverts commit edf79e9a68865845bdfb2b0d31ac7bf5ef96e998.
    
    Revert "raphael: Build GoogleCamera app"
    
    This reverts commit 81ad1591d26b56c4c403ceb0f1a617a8859ca624.
    
    Revert "raphael: overlay: Disable global mode and CDMA choices"
    
    This reverts commit b55fff7eea23368af8481cbb45752d48bb492ce1.
    
    Revert "raphael: use Proton Clang"
    
    This reverts commit 72a6b4a75fa383b6ae95ada884009e36abf53f85.
    
    Revert "raphael: Flatten APEXs for performance"
    
    This reverts commit 761adc84ba7d65d3ae52a8127342a26f23e393b7.
    
    Revert "raphael: parts: move Thermal Profiles and Custom Display Settings to system section"
    
    This reverts commit b28b17137299579cd8822c846cee5828e03f7ebd.
    
    Revert "raphael: Inject doze preference through ia for now"
    
    This reverts commit 5a8b89a8d26f4d71229da979be2d4f41e6642596.
    
    Revert "raphael: props: Enable Gboard rounded corners"
    
    This reverts commit e677fc81d36c0b73722b78d9be37431c06fbb1e4.
    
    Revert "raphael: Show "Turbo charging" instead of "Charging rapidly""
    
    This reverts commit 1a1c31ba65a1b8f12299ae909b37e1da90f5202b.
    
    Revert "raphael: Adapt the exact values for lockscreen charging info"
    
    This reverts commit 6ac624741a65e27ca748b24e570994ab483a2e79.
    
    Revert "raphael: remove MusicFX for Dolby"
    
    This reverts commit 9518980453dfb6c62010b8be15fd7cdadce2975a.
    
    Revert "raphael:Build MotoDolby v3"
    
    This reverts commit 4ab119acf3c8f0120125d437241c77a53eac3653.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.utils.FileUtils;
@@ -41,11 +42,12 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-	try {
-        DiracUtils.initialize(context);
+        try {
+            DiracUtils.initialize(context);
         } catch (Exception e) {
             Log.d(TAG, "Dirac is not present in system");
         }
+        DozeUtils.checkDozeService(context);
         PopupCameraUtils.startService(context);
         ThermalUtils.startService(context);
 
