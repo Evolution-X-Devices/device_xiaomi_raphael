fd76fc1df2ffb21478f7f26c91b59020d6fdda77
madmax7896 <madmax78968@gmail.com>
2023-09-10 10:16:28 -0300

    raphael: Import Dolby audio blobs and sepolicy
    Squashed with:
    commit b1baf8e4109edf71727c39bff18077110174dff2
    Author: Yuan Si <do4suki@gmail.com>
    Date:   Sat Sep 16 06:35:46 2023 +0800
    
        sweet: sepolicy: Address more Dolby audio denials
    
        09-16 06:33:17.209   613   613 E SELinux : avc:  denied  { find } for interface=vendor.dolby.hardware.dms::IDms sid=u:r:mediacodec:s0 pid=1262 scontext=u:r:mediacodec:s0 tcontext=u:object_r:hal_dms_hwservice:s0 tclass=hwservice_manager permissive=0
        09-16 06:33:17.209  1262  8489 E DecDynamicConfig: connectService : Connect to Dolby Native Service failed
        09-16 06:33:17.209  1262  8489 E DecDynamicConfig: checkShareConfig: Dolby service is NULL
    
        Change-Id: Ideb557073b53e7b31f6535d6053933104b455ae7
        Signed-off-by: Yuan Si <do4suki@gmail.com>
    
    commit e83b80995343c56382e43bf0656ee1e2b79ee067
    Author: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Date:   Thu Oct 12 00:02:27 2023 +0800
    
        sweet: sepolicy: Address mediacodec denials
    
        Partially taken from https://github.com/Nabu-upsidedowncake/device_xiaomi_marble/commit/ec0e9c641f40d21cdd8e7f6783903208a5005f1f and extended to hal_dms_server
    
        Change-Id: I51cfbbb2f8b9d83e74ef2f44196e8393189ca3dc
        Signed-off-by: basamaryan <basam.aryan@gmail.com>
    
    commit 0a7aab81667f137c57fa95c4661fa7e43e0e2c1c
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Fri Aug 2 20:27:52 2024 +0530
    
        sweet: Move dax-default to device tree
    
        Change-Id: Id2054bb5e61bcfe58706e32f44d37fe7524aa912
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I2d466238525d074269377a82627a816527aefd74
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,10 +64,12 @@ BOARD_HAVE_QCOM_FM := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/hidl/manifest_nfc.xml
 
--- device.mk
+++ device.mk
@@ -145,7 +145,8 @@ PRODUCT_PACKAGES += \
 
 # Audio configs
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(LOCAL_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
@@ -361,7 +362,9 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor
+    libcodec2_soft_common.vendor \
+    libcodec2_vndk.vendor \
+    libsfplugin_ccodec_utils.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
