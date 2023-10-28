6e5f8177392aef824846fceb56dfaafb0265ef26
kondors1995 <normandija1945@gmail.com>
2023-10-28 12:38:51 +0300

    raphael: Remove livedisplay

--- device.mk
+++ device.mk
@@ -364,10 +364,6 @@ $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.raphael
-
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
