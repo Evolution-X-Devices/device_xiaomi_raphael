d71e30aa7d63d25a20eeed6785a48e15552b2ebb
Joey <joey@evolution-x.org>
2024-04-10 04:13:36 +0900

    raphael: Initialize for Evolution X 9.x

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_raphael.mk
+    $(LOCAL_DIR)/lineage_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_raphael-user \
-    evolution_raphael-userdebug \
-    evolution_raphael-eng
+    lineage_raphael-user \
+    lineage_raphael-userdebug \
+    lineage_raphael-eng
