70a9c4fcc2d56269407a8bcd1d19c5a507be59f6
Joey <joey@evolution-x.org>
2024-07-05 16:40:03 +0900

    Revert "raphael: Switch to android.hardware.usb@1.3-service.dual_role_usb"
    This reverts commit de7ed54453bd438bf9b905e65b1a17e489beebb3.

--- device.mk
+++ device.mk
@@ -606,7 +606,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.0-service
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
