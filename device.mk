153c342e08f503b6c72d1785facf2ea9959be3cf
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-26 18:05:50 +0300

    raphael: Go away Megvii
    * Nuke Megvii proprietary files and build stub ones from hardware/xiaomi
    
    Change-Id: I53f8f0897b25517741a06c5962c02fe8fda61072

--- device.mk
+++ device.mk
@@ -172,7 +172,8 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    libshim_megvii \
+    libMegviiFacepp-0.5.2 \
+    libmegface \
     libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
