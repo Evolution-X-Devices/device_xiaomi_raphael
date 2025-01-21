3d0165b663177d65c0fa30af1323cbe12fedd2d6
kondors1995 <normandija1945@gmail.com>
2025-01-21 07:22:52 +0000

    raphael: Move some props to system_ext
    Resolves selinux denials

--- device.mk
+++ device.mk
@@ -511,7 +511,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext-privapp-permissions-qti.xml
 
 # Quick Tap
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_EXT_PROPERTIES  += \
     persist.columbus.model=tap7cls_coral.tflite
 
 # RCS
