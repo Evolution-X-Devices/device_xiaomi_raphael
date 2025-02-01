ac412cea59676f038df2d9ebc60f6318f4cae393
dianlujitao <dianlujitao@lineageos.org>
2025-02-01 14:33:35 +0200

    raphael: Remove OMX
     * OMX HW video codecs are no longer used after enabling Codec2
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
     * Imported media_codecs.xml, media_codecs_c2.xml, media_codecs_performance.xml from coral

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+# OMX
+TARGET_SUPPORTS_OMX_SERVICE := false
+
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
