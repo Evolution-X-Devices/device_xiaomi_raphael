5781ef811525529cd37b27180ad9c7e1f30a6ec6
kondors1995 <normandija1945@gmail.com>
2025-07-13 19:43:53 +0000

    raphael: Import audio amplifier from davinci
    HEAD https://github.com/LineageOS/android_device_xiaomi_davinci/commit/2a10f6bc3d3ab3a5b7a04060ccad9400096b46e8

--- device.mk
+++ device.mk
@@ -118,6 +118,7 @@ PRODUCT_PACKAGES += \
     audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
+    audio_amplifier.msmnile \
     libaudioroute.vendor \
     libaudiopreprocessing \
     libbatterylistener \
