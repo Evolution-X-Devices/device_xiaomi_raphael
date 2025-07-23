b64a3e951e9b49717cbeba3bd30c501c8990ea4e
Joey Huab <jhuab@edukasyon.ph>
2025-07-23 21:31:29 +0900

    Revert "raphael: Drop Mi Camera"
    This reverts commit c04df8b7b0507e7eac4639612d531e63c037255f.

--- device.mk
+++ device.mk
@@ -187,6 +187,12 @@ $(call soong_config_set,camera,override_format_from_reserved,true)
 $(call soong_config_set,camera,package_name,com.android.camera)
 $(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.raphael)
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
