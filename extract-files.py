edc447fd7b15ed17b3ee1fa95ab92758aafa263f
basamaryan <basam.aryan@gmail.com>
2025-08-05 09:45:06 +0000

    raphael: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM

--- extract-files.py
+++ extract-files.py
@@ -66,19 +66,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('remote_handle_open')
         .clear_symbol_version('remote_register_buf_attr')
         .clear_symbol_version('remote_register_buf'),
-    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so'),
-    (
-        'system_ext/lib/libwfdservice.so',
-        'system_ext/lib64/libwfdservice.so'
-    ): blob_fixup()
-        .add_needed('libaudioclient_shim.so')
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
-    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
-        .add_line_if_missing('gettid: 1'),
     (
         'vendor/lib64/libwvhidl.so',
         'vendor/lib/mediadrm/libwvdrmengine.so',
