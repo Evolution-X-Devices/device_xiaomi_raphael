0010c27c4a123d5cc2c71a73c53050a44a61b40b
RealJohnGalt <johngaltfirstrun@gmail.com>
2025-08-22 06:55:33 +0000

    raphael: revert decrease debug.hwui.target_cpu_time_percent
    There was an incorrect understanding of this prop, it is percentage of
    frame time which is cpu work.

--- device.mk
+++ device.mk
@@ -506,7 +506,7 @@ PRODUCT_PACKAGES += \
 # Enable Hint maneger for SurfaceFlinger and HWUI
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.hwui.use_hint_manager=true \
-    debug.hwui.target_cpu_time_percent=10
+    debug.hwui.target_cpu_time_percent=30
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
