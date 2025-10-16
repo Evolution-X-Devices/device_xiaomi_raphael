d6bf8f2fb0fdd150e40931f4891e9996528f9741
Joey <joey@evolution-x.org>
2025-10-16 13:08:49 +0900

    raphael: libinit: Drop fingerprint override

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -23,5 +23,4 @@ PRODUCT_MODEL := Mi 9T Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="raphael-user 11 RKQ1.200826.002 V12.5.2.0.RFKMIXM release-keys" \
-    BuildFingerprint=Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
+    BuildDesc="raphael-user 11 RKQ1.200826.002 V12.5.2.0.RFKMIXM release-keys"
