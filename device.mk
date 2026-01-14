cfc07f08aa65722f04873b2f001baca00629b4d6
Joey <joey@evolution-x.org>
2026-01-14 22:17:33 +0900

    Revert "raphael: Allow to access bootable/deprecated-ota targets"
    This reverts commit f0e395a0c84483d2e9074a2352a79b12d75f2f03.

--- device.mk
+++ device.mk
@@ -42,7 +42,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
