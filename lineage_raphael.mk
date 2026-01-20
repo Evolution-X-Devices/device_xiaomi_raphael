5883ef8381197e6396f1523b1ce2b4806d4858dc
Joey <joey@evolution-x.org>
2026-01-21 01:02:39 +0900

    raphael: Initialize for Evolution X 11.x+

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -11,6 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+EVO_BUILD_TYPE := Official
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := false
+
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
