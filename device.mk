f15cb1d93ef79142a76d505cbbbeb27489d780ad
Joey <joey@evolution-x.org>
2026-01-21 11:42:54 +0900

    raphael: Drop ViPER4Android FX support
    Revert "raphael: Add ViPER4Android FX support"
    
    This reverts commit 6f5b51379064b850c580157d46c3c89206a6d56a.

--- device.mk
+++ device.mk
@@ -647,9 +647,6 @@ PRODUCT_PACKAGES += \
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
-# Viper
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 # VNDK
 PRODUCT_PACKAGES += \
     libcrypto-v33 \
